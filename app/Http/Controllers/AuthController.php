1ca7c796a4afa5c9b9f73751525646d9f1d87707
shencanet <santi.dionis@gmail.com>
2022-08-06 16:42:55 +0200

    ADD: controller auth

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator as Validator;
+use Symfony\Component\HttpFoundation\Response;
+use Tymon\JWTAuth\Facades\JWTAuth;
+
+class AuthController extends Controller
+{
+    const ROLE_USER = 1;
+    public function register(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:100',
+            'username' => 'required|string|max:100',
+            'steamUsername' => 'required|string|max:100',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:6',
+        ]);
+        if ($validator->fails()) {
+            return response()->json(
+                [
+                    "success" => true,
+                    "message" => $validator->errors()
+                ],
+                400
+            );
+        }
+        $user = User::create([
+            'name' => $request->input('name'),
+            'username' => $request->input('username'),
+            'steamUsername' => $request->input('steamUsername'),
+            'email' => $request->input('email'),
+            'password' => bcrypt($request->password)
+        ]);
+
+        $user->roles()->attach(self::ROLE_USER);
+
+        $token = JWTAuth::fromUser($user);
+        return response()->json(compact('user', 'token'), 201);
+    }
+
+    public function login(Request $request)
+    {
+        $input = $request->only('email', 'password');
+        $jwt_token = null;
+
+        if (!$jwt_token = JWTAuth::attempt($input)) {
+            return response()->json(
+                [
+                    'success' => false,
+                    'message' => 'Invalid Email or Password',
+                ],
+                Response::HTTP_UNAUTHORIZED
+            );
+        }
+
+        return response()->json([
+            'success' => true,
+            'token' => $jwt_token,
+        ]);
+    }
+    public function logout(Request $request)
+    {
+        $this->validate($request, [
+            'token' => 'required'
+        ]);
+        try {
+            JWTAuth::invalidate($request->token);
+            return response()->json([
+                'success' => true,
+                'message' => 'User logged out successfully'
+            ]);
+        } catch (\Exception $exception) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Sorry, the user cannot be logged out'
+            ], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+    }
+    public function me()
+    {
+        return response()->json(auth()->user());;
+    }
+}
